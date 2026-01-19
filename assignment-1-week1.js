828cde2c42b770ed644a9d5a4aae4b4d31118539
xliu715-svg <xliu715@gmail.com>
2026-01-19 13:58:16 +0100

    Add personal information and age calculation

--- assignment-1-week1.js
+++ assignment-1-week1.js
@@ -1,5 +1,30 @@
+//1st part
 const firstName = "Xiaohui";
 const favoriteNumber  = 8;
 const likesCoding = true;
 
+
 console.log(`Hi, my name is ${firstName}, my favourite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`);
+
+
+//2nd part
+const birthYear = 1989;
+const currentYear = 2026;
+const age = currentYear - birthYear;
+
+
+console.log(`I am ${age} years old.`);
+
+
+//3rd part
+let arr = ["noodles", "meatballs", "salad"];
+let obj = {
+   firstName,
+   favoriteNumber,
+   likesCoding,
+   arr
+};
+
+
+console.log(obj);
+console.log(typeof firstName);
