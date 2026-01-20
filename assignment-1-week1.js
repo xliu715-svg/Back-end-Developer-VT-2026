f78371c353bed7de7d8e1154c466dbe0a948a573
xliu715-svg <xliu715@gmail.com>
2026-01-20 10:53:24 +0100

    Initialize homework.js with instructions and variables
    Added instructions and initial variable declarations.

--- assignment-1-week1.js
+++ assignment-1-week1.js
@@ -1,3 +1,69 @@
+// Week 1
+
+
+// Homework: Introduction to Node.js & Variables
+
+
+// Instructions:
+
+// 1. Create a file called homework.js.
+
+// 2. Make 3 variables about yourself:
+
+// · firstName (string)
+
+// · favoriteNumber (number)
+
+// · likesCoding (boolean)
+
+// 3. Print them in a full sentence using template literals:
+
+// console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`);
+
+// 4. Run the script in Node.js and take a screenshot of the output.
+
+// 5. (Optional) Push the file to GitHub.
+
+// 6. Complete the other challenges if you want the challenge.
+
+
+// Write your code here:
+
+
+// Paste your terminal output (screenshot):
+
+
+// Extra practice:
+
+// 1. Add two more variables:
+
+// o birthYear (number)
+
+// o currentYear (number)
+
+// 2. Calculate your age using subtraction.
+
+// 3. Print another sentence with your age:
+
+
+// Advanced:
+
+
+// 1. Make an array of your 3 foods.
+
+// 2. Make an object called “student” that contains:
+
+// a. Your name
+
+// b. Favourite number
+
+// c. likesCoding
+
+// d. the array of foods
+
+// 3. Print the whole object and also one of the properties.
+
+
 //1st part
 const firstName = "Xiaohui";
 const favoriteNumber  = 8;
